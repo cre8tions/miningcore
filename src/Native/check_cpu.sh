a4536ef2e2abd7520c6a0a2b11f2b00b927c10bc
Joe Kushner <joe@cre8tions.net>
2024-01-16 00:29:25 -0800

    Update build script, config.json, and check_cpu.sh

--- src/Native/check_cpu.sh
+++ src/Native/check_cpu.sh
@@ -7,7 +7,7 @@ check_mac() {
    avx) sysctl -n machdep.cpu.features | grep -i avx >/dev/null ;;
    avx2) sysctl -n machdep.cpu.features | grep -i avx2 >/dev/null ;;
    amd) sysctl -n machdep.cpu.vendor | grep -i amd >/dev/null ;;
-   amdnew) sysctl -n machdep.cpu.vendor | grep -i amd && test $(sysctl -n machdep.cpu.family) -ge 23 ;;
+amdnew) sysctl -n machdep.cpu.vendor | grep -i amd && test "$(sysctl -n machdep.cpu.family)" -ge 23 ;;
    intel) sysctl -n machdep.cpu.vendor | grep -i intel >/dev/null ;;
    sse2) sysctl -n machdep.cpu.features | grep -i sse2 >/dev/null ;;
    sse3) sysctl -n machdep.cpu.features | grep -i sse3 >/dev/null ;;
@@ -28,7 +28,7 @@ check_linux() {
    avx) grep -w 'avx' /proc/cpuinfo >/dev/null ;;
    avx2) grep -w 'avx2' /proc/cpuinfo >/dev/null ;;
    amd) grep -i amd /proc/cpuinfo >/dev/null ;;
-   amdnew) grep -i amd /proc/cpuinfo >/dev/null && test $(awk '/cpu family/ && $NF~/^[0-9]*$/ {print $NF}' /proc/cpuinfo | head -n1) -ge 23 >/dev/null ;;
+   amdnew) grep -i amd /proc/cpuinfo >/dev/null && test "$(awk '/cpu family/ && $NF~/^[0-9]*$/ {print $NF}' /proc/cpuinfo | head -n1)" -ge 23 >/dev/null ;;
    intel) grep -i intel /proc/cpuinfo >/dev/null ;;
    sse2) grep -w 'sse2' /proc/cpuinfo >/dev/null ;;
    sse3) grep -w 'sse3' /proc/cpuinfo >/dev/null ;;
