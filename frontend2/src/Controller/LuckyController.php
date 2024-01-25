80a7b20591a35172e42a78a5ec1f675ed78f38ee
Joe Kushner <joe@cre8tions.net>
2024-01-25 13:45:33 -0800

    symfony

---
+++ frontend2/src/Controller/LuckyController.php
@@ -0,0 +1,40 @@
+<?php
+// src/Controller/LuckyController.php
+/**
+ * Namespace declaration for the LuckyController class.
+ *
+ * This namespace declaration specifies the location of the LuckyController class
+ * within the application's directory structure.
+ *
+ * @package App\Controller
+ */
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * Controller class for generating a lucky number.
+ */
+class LuckyController extends AbstractController
+{
+    /**
+     * Generates a random lucky number.
+     *
+     * @return Response The HTTP response containing the lucky number.
+     */
+    #[Route('/lucky/number')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        // return new Response(
+        //     '<html><body>Lucky number: '.$number.'</body></html>'
+        // );
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
